372c5e298dd23be1f0105d4bb28e97586f96bd5b
Viacheslav Brovko <vbrovko@turnkey-lender.com>
2025-06-01 22:58:52 +0200

    Update date when update Biggest day streak achievement

--- backend/Veloci.Data/Achievements/BiggestDayStreakAchievement.cs
+++ backend/Veloci.Data/Achievements/BiggestDayStreakAchievement.cs
@@ -45,5 +45,6 @@ public class BiggestDayStreakAchievement : IAchievementSelfCheck
             return;
 
         currentAchievement.Pilot = pilotWithBiggestDayStreak;
+        currentAchievement.Date = DateTime.Now;
     }
 }
