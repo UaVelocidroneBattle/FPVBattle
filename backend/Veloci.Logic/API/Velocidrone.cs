239193a2303c90887368e7bd55b343ce691b16cf
Viacheslav Brovko <vbrovko@turnkey-lender.com>
2025-06-27 00:58:05 +0200

    Use HttpClient instead of RestSharp

--- backend/Veloci.Logic/API/Velocidrone.cs
+++ backend/Veloci.Logic/API/Velocidrone.cs
@@ -1,5 +1,7 @@
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
 using Microsoft.Extensions.Configuration;
-using RestSharp;
 using Veloci.Logic.API.Dto;
 using Veloci.Logic.Services;
 
@@ -7,10 +9,16 @@ namespace Veloci.Logic.API;
 
 public class Velocidrone
 {
+    private readonly HttpClient _httpClient;
     private readonly string? _apiToken;
 
     public Velocidrone(IConfiguration configuration)
     {
+        _httpClient = new HttpClient
+        {
+            BaseAddress = new Uri(VelocidroneApiConstants.BaseUrl)
+        };
+
         _apiToken = configuration.GetSection("API:Token").Value;
     }
 
@@ -19,28 +27,34 @@ public class Velocidrone
         var payload = $"track_id={trackId}&sim_version=1.16&offset=0&count=10000&race_mode=6";
         var postData = $"post_data={Uri.EscapeDataString(payload)}";
 
-        var response = await DoRequestAsync<LeaderboardDto>("api/leaderboard", Method.Post, postData);
+        var response = await DoRequestAsync<LeaderboardDto>("api/leaderboard", HttpMethod.Post, postData);
         return response.tracktimes;
     }
 
-    private async Task<T> DoRequestAsync<T>(string uri, Method method, string? formData = null)
+    private async Task<T> DoRequestAsync<T>(string uri, HttpMethod method, string? formData = null)
     {
-        var client = new RestClient(VelocidroneApiConstants.BaseUrl);
-        var request = new RestRequest(uri, method);
-        request.AddHeader("Authorization", $"Bearer {_apiToken}");
+        var request = new HttpRequestMessage(method, uri);
+        request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", _apiToken);
 
-        if (formData != null)
+        if (formData is not null)
         {
-            request.AddParameter("application/x-www-form-urlencoded", formData, ParameterType.RequestBody);
+            request.Content = new StringContent(formData, Encoding.UTF8, "application/x-www-form-urlencoded");
         }
 
-        var response = await client.ExecuteAsync<T>(request);
+        var response = await _httpClient.SendAsync(request);
 
-        if (response.IsSuccessful && response.Data != null)
+        if (response.IsSuccessStatusCode)
         {
-            return response.Data;
+            var content = await response.Content.ReadAsStringAsync();
+            var data = JsonSerializer.Deserialize<T>(content);
+
+            if (data is not null)
+                return data;
+
+            throw new Exception("Response deserialized as null.");
         }
 
-        throw new Exception(response.ErrorMessage);
+        var error = await response.Content.ReadAsStringAsync();
+        throw new Exception($"Request failed: {response.StatusCode}, {error}");
     }
 }
