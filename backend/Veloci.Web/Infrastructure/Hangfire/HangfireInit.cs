0542830441cf809c1e78dfaa7a0d21323df56a1a
Viacheslav Brovko <vbrovko@turnkey-lender.com>
2025-07-31 23:53:12 +0200

    Add telegram command for manual track change

--- backend/Veloci.Web/Infrastructure/Hangfire/HangfireInit.cs
+++ backend/Veloci.Web/Infrastructure/Hangfire/HangfireInit.cs
@@ -11,12 +11,12 @@ public class HangfireInit
     public static void InitRecurrentJobs(IConfiguration configuration)
     {
         Log.Information("Initializing Hangfire recurring jobs");
-        
+
         using (var connection = JobStorage.Current.GetConnection())
         {
             var existingJobs = connection.GetRecurringJobs();
             Log.Information("Removing {JobCount} existing recurring jobs", existingJobs.Count);
-            
+
             foreach (var recurringJob in existingJobs)
             {
                 RecurringJob.RemoveIfExists(recurringJob.Id);
@@ -24,6 +24,7 @@ public class HangfireInit
         }
 
         Log.Information("Setting up daily competition schedule recurring jobs");
+
         RecurringJob.AddOrUpdate<CompetitionService>("Day streak potential lose", x => x.DayStreakPotentialLoseNotification(), "5 14 * * *");
         RecurringJob.AddOrUpdate<CompetitionConductor>("Vote reminder", x => x.VoteReminder(), "30 14 * * *");
         RecurringJob.AddOrUpdate<CompetitionConductor>("Stop poll", x => x.StopPollAsync(), "58 14 * * *");
@@ -33,12 +34,14 @@ public class HangfireInit
         RecurringJob.AddOrUpdate<CompetitionService>("Day streak achievements", x => x.PublishDayStreakAchievements(), "5 15 * * *");
 
         Log.Information("Setting up continuous monitoring recurring jobs");
+
         RecurringJob.AddOrUpdate<CompetitionService>("Update results", x => x.UpdateResultsAsync(), "*/10 * * * *");
         RecurringJob.AddOrUpdate<CompetitionService>("Publish current leaderboard", x => x.PublishCurrentLeaderboardAsync(), "1 */2 * * *");
 
         Log.Information("Setting up yearly recurring jobs");
+
         RecurringJob.AddOrUpdate<YearResultsService>("Year results", x => x.Publish(), "15 11 2 1 *");
-        
+
         Log.Information("Hangfire recurring job initialization completed");
     }
 }
